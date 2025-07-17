e8551166fad17bbe36d8a3edbd2065c275299697
farah-telli <farah.telli@esprit.tn>
2025-07-17 14:42:26 +0100

    first commit

---
+++ src/app/admin/avis-list/avis-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-avis-list',
+  imports: [],
+  templateUrl: './avis-list.component.html',
+  styleUrl: './avis-list.component.css'
+})
+export class AvisListComponent {
+
+}
